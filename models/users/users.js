84d7038bdfeb78cd9cb3861668b1c2430e3ba2fa
Solod Sergey <104456968+Solod-S@users.noreply.github.com>
2023-04-04 22:37:08 +0300

    auth
    - start working on auth end-point

---
+++ models/users/users.js
@@ -0,0 +1,85 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const mongooseErrorHandler = require("../helpers/handleMongooseError");
+
+const emailRegexp = /^\w+([-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+    avatarURL: {
+      type: String,
+      required: false,
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    subscription: {
+      type: Boolean,
+      default: false,
+    },
+    favorite: {
+      type: Array,
+      default: [],
+    },
+    verificationCode: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", mongooseErrorHandler);
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const emailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const subscribeSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+});
+
+const schemas = {
+  registerSchema,
+  emailSchema,
+  loginSchema,
+  subscribeSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
---
+++ routes/api/auth/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const crtl = require("../../controllers/users/auth");
+
+const { validateBody, authenticate, upload } = require("../../middlewares");
+
+const { schemas } = require("../../models/users");
+
+const router = express.Router();
+
+//  sign-up
+router.post("/register", validateBody(schemas.registerSchema), crtl.register);
+
+module.exports = router;
