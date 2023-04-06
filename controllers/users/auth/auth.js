42be6dd1513ae9dab5217b26a52b103e85cf7615
Solod Sergey <104456968+Solod-S@users.noreply.github.com>
2023-04-06 07:19:19 +0300

    auth
    -trying fixing problen in auth endpoint

---
+++ controllers/users/auth/auth.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../../models/users");
+const jwt = require("jsonwebtoken");
+const uuid = require("uuid");
+
+const { controllersWraper, sendEmail } = require("../../helpers");
+const { HttpError } = require("../../routes/errors/HttpErrors");
+
+const { SECRET_KEY, BASE_URL } = process.env;
+
+const register = async (rec, res) => {
+  const { email, password } = rec.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "This email already in use");
+  }
+  const hashPassword = await bcrypt.hash(password, 10);
+  const verificationCode = uuid.v4();
+
+  const newUser = await User.create({
+    ...rec.body,
+    password: hashPassword,
+    verificationCode,
+  });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/auth/verify/${verificationCode}" >Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
+
+  res.status(201).json({
+    email: newUser.email,
+    name: newUser.name,
+  });
+};
+
+module.exports = {
+  register: controllersWraper(register),
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const crtl = require("../../controllers/users/auth");
 
-const { validateBody, authenticate, upload } = require("../../middlewares");
+const { validateBody } = require("../../middlewares");
 
 const { schemas } = require("../../models/users");
 
